00e7433bc596254172a3b4da4d9be7a6219079f9
ahmadhamzh <ahmadhamzh@ymail.com>
2021-09-02 16:59:43 +0300

    lab 05 not complete

--- src/index.js
+++ src/index.js
@@ -32,9 +32,9 @@ import Register from "views/examples/Register.js";
 ReactDOM.render(
   <BrowserRouter>
     <Switch>
-      <Route path="/" exact render={props => <Index {...props} />} />
+      <Route path="/" exact render={props => <profile {...props} />} />
       <Route
-        path="/landing-page"
+        path="/portfolio"
         exact
         render={props => <Landing {...props} />}
       />
@@ -50,6 +50,11 @@ ReactDOM.render(
         render={props => <Register {...props} />}
       />
       <Redirect to="/" />
+    <Route
+      path="/how-to"
+      exact
+      render={props => <Index {...props} />}
+    />
     </Switch>
   </BrowserRouter>,
   document.getElementById("root")
